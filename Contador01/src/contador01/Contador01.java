33e13242860556cce9d540f92da7d272d86fbad4
Reginaldo Batista <89160333+RegiBatista@users.noreply.github.com>
2021-09-08 10:06:46 -0300

    Add files via upload

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,15 @@
+package contador01;
+
+public class Contador01 {
+
+    
+    public static void main(String[] args) {
+       
+        int cc = 1;
+        while (cc<=20) {
+            System.out.println("Camabalhota" + cc);
+            cc++;
+        }
+    }
+    
+}
